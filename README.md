e1b3f89d8d1e46d26d70777e19ef0e143d70d4ae
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-01-06 00:43:32 -0500

    Orders microservice - SECTION 06

---
+++ README.md
@@ -0,0 +1,15 @@
+# Orders MicroService
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+```
+docker compose up -d
+```
+
+## Development pasos
+
+1. Clonar el proyecto
+2. Crear un archivo `.env` basado en el archivo `.env.template`
+3. Levantar la base de datos con `docker compose up -d`
+4. Levantar el proyecto con `npm run start:dev`
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Orders-Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`Running on port ${envs.port}`);
+}
+bootstrap();
